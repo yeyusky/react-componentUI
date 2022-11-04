364602b8d97d5807d9b17c962184ae0c3ea9626b
qinlingze <327853133@qq.com>
2022-11-04 09:19:29 +0800

    提交UI项目配置文件

--- src/components/clock/clock.js
+++ src/components/clock/clock.js
@@ -7,7 +7,7 @@ import './clock.css'
 export default class Clock extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {time: new Date(),visable:false}
+        this.state = {time: new Date(),visable:false,visables:false}
     }
 
     componentDidMount() {
@@ -23,9 +23,14 @@ export default class Clock extends React.Component{
             time: new Date()
         })
     }
-    alerts() {
+    open() {
         this.setState({
             visable:true
+        })
+    }
+    opens() {
+        this.setState({
+            visables:true
         })
     }
     closeFun() {
@@ -44,25 +49,25 @@ export default class Clock extends React.Component{
                 <h1 className='clockTitle'>Hello, world!</h1>
                 <h2 className='clockContent'>北京时间 {this.state.time.toLocaleTimeString()}</h2>
                 <div style={{display:'flex',flex:1,justifyContent:'center',alignItems:'center',margin:'15px 0'}}>
-                    <Button title='圆' disabled='disabled' circle='circle' type='primary' size='large'></Button>
-                    <Button title='圆' circle='circle' type='success' size='small'></Button>
-                    <Button title='D' disabled='disabled' circle='circle' type='' size='mini'></Button>
-                    <Button title='圆' disabled='disabled' circle='circle' type='warning' size='medium'></Button>
-                    <Button title='C' circle='circle' type='danger' size='small'></Button>
-                    <Button title='圆' circle='circle'></Button>
-                    <Button title='A' onClick={() => this.alerts()} circle='circle' type='danger' size='small'></Button>
-                    <Button title='按钮' type='primary' size='large'></Button>
-                    <Button title='按钮' type='success' size='small'></Button>
-                    <Button title='钮'  circle='circle' type='' size='mini'></Button>
-                    <Button title='按钮' disabled='disabled' type='warning' size='medium'></Button>
-                    <Button title='C' type='danger' size='small'></Button>
-                    <Button title='圆' type='primary'></Button>
-                    <Button title='A' onClick={() => this.alerts()}  type='danger' size='small'></Button>
-                    <Dialog width='80%' height='400px' btnAlign='center' onClose={() => this.closeFun()} onOk={() => this.handleClickOk()} title='按钮弹出层' mark='true' visable={this.state.visable}>
-                        <div style={{padding:'10px'}}><input value='这是一个测试' style={{width:'200px',height:'60px'}}></input><Button title='按钮' type='primary' size='large'></Button>
+                    <Button title='Dialog按钮' onClick={() => this.open()}  type='primary' size='small'></Button>
+                    <Button title='Dialog按钮' onClick={() => this.opens()}  type='primary' size='small'></Button>
+                    <Dialog width='90%' type='1' height='80%' btnAlign='center' onClose={() => this.closeFun()} onOk={() => this.handleClickOk()} title='按钮弹出层' mark='true' visable={this.state.visable}>
+                        <Button title='圆' circle='circle' type='success' size='small'></Button>
+                        <Button title='D' disabled='disabled' circle='circle' type='' size='mini'></Button>
+                        <Button title='圆' disabled='disabled' circle='circle' type='warning' size='medium'></Button>
+                        <Button title='C' circle='circle' type='danger' size='small'></Button>
+                        <Button title='圆' circle='circle'></Button>
+                        <Button title='A' onClick={() => this.alerts()} circle='circle' type='danger' size='small'></Button>
+                        <Button title='按钮' type='primary' size='large'></Button>
                         <Button title='按钮' type='success' size='small'></Button>
                         <Button title='钮'  circle='circle' type='' size='mini'></Button>
-                        <Button title='按钮' disabled='disabled' type='warning' size='medium'></Button></div>
+                        <Button title='按钮' disabled='disabled' type='warning' size='medium'></Button>
+                        <Button title='C' type='danger' size='small'></Button>
+                        <Button title='圆' type='primary'></Button>
+                        <Button title='A'   type='danger' size='small'></Button>
+                    </Dialog>
+                    <Dialog  type='2' btnAlign='center' onClose={() => this.closeFun()} onOk={() => this.handleClickOk()} title='按钮弹出层' mark='true' visable={this.state.visables}>
+                        这是一个提示
                     </Dialog>
                 </div>
                 <header className="App-header">
--- src/components/dialog/dialog.js
+++ src/components/dialog/dialog.js
@@ -24,13 +24,60 @@ export default class Clock extends React.Component{
         if(!this.props.onOk) return;
         this.props.onOk();
     }
+    verifyFun = (val) => {
+        let w = document.body.clientWidth;
+        let h = document.body.clientHeight;
+        if(this.props.height && val === 'h') {
+            if(this.props.height.indexOf('px')){
+                if(h-Number(this.props.height.split('px')[0]) < 0) {
+                    return '100%'
+                }else{
+                    return this.props.height
+                }
+            }else if(this.props.height.indexOf('%')){
+                if(Number(this.props.height.split('%')[0]) - 100 >0) {
+                    return '100%'
+                }else{
+                    return this.props.height
+                }
+            } 
+        }
+        if(this.props.width && val === 'w') {
+            if(this.props.width.indexOf('px')){
+                if(w-Number(this.props.width.split('px')[0]) < 0) {
+                    return '100%'
+                }else{
+                    return this.props.width
+                }
+            }else if(this.props.width.indexOf('%')){
+                if(Number(this.props.width.split('%')[0]) - 100 >0) {
+                    return '100%'
+                }else{
+                    return this.props.width
+                }
+            } 
+        }
+    }
     render() {
-        const height = ``
+        const height = this.verifyFun('h');
+        const width = this.verifyFun('w');
         // const btnClassName = `btn${this.props.type ? ' btn_'+this.props.type : ''}${this.props.size ? ' btn_'+this.props.size : ''}${this.props.disabled ? ' btn_disabled' : ''}${this.props.circle && this.props.title.length === 1 ? ' btn_circle' : ''}`;
         return ( 
             <div>
-                <div className='dialog' style={{opacity:this.props.visable ? '1' : '0',top:this.props.height ? 'calc((100% - '+this.props.height+')/2)' : '50px',left:this.props.width ? 'calc((100% - '+this.props.width+')/2)' : '25%',height:this.props.height ? this.props.visable ? this.props.height : '0px' : this.props.visable ? '300px' : '0px',width:this.props.width ? this.props.width : '50%'}}>
-                    <div className='dialog-title'>{this.props.title}<div className='closeBtn' onClick={this.handleClickClose} >X</div></div>
+                <div className='dialog' style={{display:this.props.visable&&this.props.type==='1'?'block':'none',borderRadius:height==='100%'&&width==='100%'?'0':'10px',opacity:this.props.visable ? '1' : '0',top:this.props.height ? 'calc((100% - '+height+')/2)' : '50px',left:this.props.width ? 'calc((100% - '+width+')/2)' : '25%',height:this.props.height ? this.props.visable ? height : '0px' : this.props.visable ? '300px' : '0px',width:this.props.width ? width : '50%'}}>
+                    <div className='dialog-title'>{this.props.title?this.props.title:'标题'}<div className='closeBtn' onClick={this.handleClickClose} >X</div></div>
+                    <div className='dialog-content'>
+                        {this.props.children}
+                    </div>
+                    <div className='dialog-footer'>
+                        <div className='dialog-btnBox' style={{textAlign:this.props.btnAlign ? this.props.btnAlign : 'center'}}>
+                            <Button title='取消' size='small' onClick={this.handleClickClose}></Button>
+                            <Button title='确定' size='small' type='primary' onClick={this.handleClickOK}></Button>
+                        </div>
+                    </div>
+                </div>
+                <div className='dialog' style={{display:this.props.visable&&this.props.type==='2'?'block':'none',opacity:this.props.visable ? '1' : '0',top:'50px',left:'calc((100% - 320px)/2)',minHeight:'220px',width:'320px'}}>
+                    <div className='dialog-title'>{this.props.title?this.props.title:'提示'}<div className='closeBtn' onClick={this.handleClickClose} >X</div></div>
                     <div className='dialog-content'>
                         {this.props.children}
                     </div>
